ee5ee82f6b59cfd28b3596b19b18623281ae2fd7
Ken Tran <kentran24797@gmail.com>
2018-01-17 17:00:17 +0700

    update login layout

--- SingNow/app/src/main/java/com/idroid/pakandroid/ptit/singnow/LoginActivity.java
+++ SingNow/app/src/main/java/com/idroid/pakandroid/ptit/singnow/LoginActivity.java
@@ -13,6 +13,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login1);
+        setContentView(R.layout.activity_login);
     }
 }
