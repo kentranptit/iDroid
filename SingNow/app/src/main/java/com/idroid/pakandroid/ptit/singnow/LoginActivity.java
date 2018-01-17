551bbada58e8b124e70c9e95bc6c58926fecb2aa
Ken Tran <kentran24797@gmail.com>
2018-01-17 13:45:08 +0700

    background color

---
+++ SingNow/app/src/main/java/com/idroid/pakandroid/ptit/singnow/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.idroid.pakandroid.ptit.singnow;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 1/17/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
